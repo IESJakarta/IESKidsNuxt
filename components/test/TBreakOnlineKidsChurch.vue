4f1ddb7cbea79c4512c833ee607903f0a14a49eb
IESKids <63225321+IESKids@users.noreply.github.com>
2020-12-24 00:30:47 +0700

    Add files via upload

--- components/test/TBreakOnlineKidsChurch.vue
+++ components/test/TBreakOnlineKidsChurch.vue
@@ -1,64 +1,116 @@
 <template>
-<div>
-    <div v-for = "link in links" v-bind:key = "link.SortOrder" 
-    class="flex-container_1">
-        <div><br></div>
-            
-            <!-- this is the head -->
-            <div v-if="link.Category === 'Header Content'">
-            <div class="flex-container_3A" style="text-align: center"
-                :style="{ 'backgroundColor': link.Week_Card_Color }">
-            <h2>{{ link.SectionTitle }} <br>
-            {{ link.Week }} </h2>
-            <div v-if="link.SectionSubTitle"><h3> {{ link.SectionSubTitle }} </h3></div>
-            <div v-if="link.Text"><p> {{ link.Text }} </p></div>
-            <div v-if="link.Graphic">
-                <div class="flex-item_1">
-                    <a v-bind:href="link.Graphic_Link + '&ref=Kids_Break_Header_Graphic_Link_' + link.Week"
-                        target="_blank">
-                        <img style='height: auto; width: 100%; object-fit: contain'
-                            :src="link.Graphic_Link">
-                    </a>
-                </div>
-            </div>
+	<div>
+		<div
+			v-for="link in links"
+			v-bind:key="link.SortOrder"
+			class="flex-container_1"
+		>
+			<div><br /></div>
 
+			<!-- this is the head -->
+			<div v-if="link.Category === 'Header Content'">
+				<div
+					class="flex-container_3A"
+					style="text-align: center"
+					:style="{ backgroundColor: link.Week_Card_Color }"
+				>
+					<h2>
+						{{ link.SectionTitle }} <br />
+						{{ link.Week }}
+					</h2>
+					<div v-if="link.SectionSubTitle">
+						<h3>{{ link.SectionSubTitle }}</h3>
+					</div>
+					<div v-if="link.Text">
+						<p>{{ link.Text }}</p>
+					</div>
+					<div v-if="link.Graphic">
+						<div class="flex-item_1">
+							<a
+								v-bind:href="
+									link.Graphic_Link +
+									'&ref=Kids_Break_Header_Graphic_Link_' +
+									link.Week
+								"
+								target="_blank"
+							>
+								<img
+									style="height: auto; width: 100%; object-fit: contain"
+									:src="link.Graphic_Link"
+								/>
+							</a>
+						</div>
+					</div>
+					<div v-if="link.HTMLContent">
+						<div class="flex-item_1">
+							<p>
+								<span style="font-size: 80%" v-html="link.HTMLContent"></span>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div><p /></div>
 
-            </div>
-            <div><p /></div>
+				<div
+					class="flex-container_2A"
+					:style="{ backgroundColor: link.Week_Card_Color }"
+				>
+					<div class="grid_Internal">
+						<div v-for="link in links.slice(1)" v-bind:key="link.SortOrder">
+							<div v-if="link.Category === 'Section Content'">
+								<div
+									class="flex-container_3A"
+									style="text-align: center"
+									:style="{ backgroundColor: link.Curriculum_level_box_Color }"
+								>
+                                    <div class="flex-item_1">
 
-        <div class="flex-container_3A" style="text-align: center"
-                :style="{ 'backgroundColor': link.Week_Card_Color }">
-            <div class="grid_Internal">
-               
-        <div v-for = "link in links.slice(1)" v-bind:key = "link.SortOrder">
-                <div v-if="link.Category === 'Section Content'">
-                <div class="flex-container_3A" style="text-align: center"
-                :style="{ 'backgroundColor': link.Curriculum_level_box_Color }">
-                    <h2>{{ link.SectionTitle }} <br>
-                    {{ link.Week }} </h2>
-                    <div v-if="link.SectionSubTitle"><h3> {{ link.SectionSubTitle }} </h3></div>
-                    <div v-if="link.Text"><p> {{ link.Text }} </p></div>
-                    <div v-if="link.Graphic">
-                        <div class="flex-item_1">
-                            <a v-bind:href="link.Graphic_Link + '&ref=Kids_Break_Header_Graphic_Link_' + link.Week"
-                                target="_blank">
-                                <img style='height: auto; width: 100%; object-fit: contain'
-                                    :src="link.Graphic_Link">
-                            </a>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-        </div>
-        </div>
-        </div>
-        </div>
-
-    </div>
-
-</div>
+									<h2>
+										{{ link.SectionTitle }} <br />
+										{{ link.Week }}
+									</h2>
+									<div v-if="link.SectionSubTitle">
+										<h3>{{ link.SectionSubTitle }}</h3>
+									</div>
+                                    </div>
+									<div v-if="link.Text" class="flex-item_2">
+										<p>{{ link.Text }}</p>
+									</div>
+									<div v-if="link.Graphic">
+										<div class="flex-item_1">
+											<a
+												v-bind:href="
+													link.Graphic_Link +
+													'&ref=Kids_Break_Header_Graphic_Link_' +
+													link.Week
+												"
+												target="_blank"
+											>
+												<img
+													style="height: auto; width: 100%; object-fit: contain"
+													:src="link.Graphic_Link"
+												/>
+											</a>
+										</div>
+									</div>
+									<div v-if="link.HTMLContent">
+										<div class="flex-item_2">
+											<p>
+												<span
+													style="font-size: 80%"
+													v-html="link.HTMLContent"
+												></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 
@@ -87,5 +139,4 @@ export default {
 
 <style>
 @import "./../../assets/css/stylesheet.css";
-
 </style>
