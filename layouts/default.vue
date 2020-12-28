0585cb69b586051e2074f0be9bc81dfa41782d92
IESKids <63225321+IESKids@users.noreply.github.com>
2020-12-28 21:52:53 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="flex-container_Base_A">
+    <div class="topBar">
+      <LogoPurpose />
+    </div>
     <Hero />
     <Nuxt />
   </div>
@@ -7,5 +10,10 @@
 
 <style>
     @import '~assets/css/stylesheet.css';
-
+.topBar {
+ overflow: hidden;
+ position: fixed;
+ top: 0;
+ width: 100%; 
+}
 </style>
