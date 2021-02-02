48505cef07781f2d9d9da34b45d230721a15de96
IESKids <63225321+IESKids@users.noreply.github.com>
2021-02-02 22:20:08 +0700

    Add files via upload

---
+++ pages/Kids_LG_Reg/index.vue
@@ -0,0 +1,146 @@
+
+
+<template>
+	<div style="background-color: #d8dce0">
+		<br />
+
+		<div
+			class="flex-container__Base_A"
+			style="
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+			"
+		>
+			<div
+				class="flex-container_2A"
+				style="text-align: center; background-color: #cee5f1"
+			>
+				<br />
+				<br />
+
+				<h2>
+					<h2a> IES Kids LG Info & Registration</h2a>
+				</h2>
+				<div style="text-align: center; width: 80%">
+					<p>
+						<!--<strong>Registration for the upcomming round of IES Kids LGs has already closed.</strong> -->
+						<strong>
+							Registration is open for 2 IES Kids LifeGroups for ages 6-10!
+						</strong>
+					</p>
+					<p>
+						These LifeGroups will be held on the Zoom platform. One each on
+						Wednesdays and Thursdays. Please choose the group that best suits
+						your child’s schedule. Register as soon as possible; groups are
+						limited to 12 participants each. As groups are filled registration
+						will be closed.
+					</p>
+				</div>
+				<div class="grid_Internal">
+					<div class="flex-item_SpText" style="background-color: #fe875f">
+						<div class="grid_Internal">
+							<div>
+								<img
+									src="~/assets/graphics/Kids_LG_Reg/WisdomLGLogo.png"
+									alt="Truth LG for Kids Every Wednesday"
+									width="auto"
+									height="250px"
+								/>
+							</div>
+							<div>
+								<h2>Wisdom LifeGroup</h2>
+								<br />
+								<strong>Wednesdays</strong>, 4PM<br />
+								<strong>Start Date</strong>: 10 February 2021<br />
+								<strong>End Date</strong>: 17 March 2021<br />
+								<strong>LG Registration Full</strong>
+								<a
+									href="https://iesjakarta.churchcenter.com/registrations/events/661302&ref=LinkToWisdomLG"
+									target="_blank"
+									><strong>CLICK HERE TO REGISTER</strong></a
+								>
+							</div>
+						</div>
+					</div>
+
+					<div class="flex-item_SpText" style="background-color: #fed75f">
+						<div class="grid_Internal">
+							<div>
+								<img
+									src="~/assets/graphics/Kids_LG_Reg/TruthLGLogo.png"
+									alt="Truth LG for Kids Every Thursday"
+									width="auto"
+									height="250px"
+								/>
+							</div>
+							<div>
+								<h2>Truth LifeGroup</h2>
+								<br />
+								<strong>Thursdays</strong>, 4PM<br />
+								<strong>Start Date</strong>: 11 February 2021<br />
+								<strong>End Date</strong>: 18 March 2021<br />
+								<strong>LG Registration Full</strong>
+								<a
+									href="https://iesjakarta.churchcenter.com/registrations/events/661304&ref=LinkToTruthLG"
+									target="_blank"
+									><strong>CLICK HERE TO REGISTER</strong></a
+								>
+							</div>
+						</div>
+					</div>
+
+					<!--
+                    <div class="flex-item_SpText" style="background-color:#d66786">
+                        <div class="grid_Internal">
+                            <div>
+
+                                <img src="Faith LG Log.png" alt="Faith LG for Kids Every Friday" width="auto"
+                                    height="250px">
+                            </div>
+                            <div>
+                                <h2>Faith LifeGroup</h2><br>
+                                Every Friday, 4PM<br>
+                                <strong>Start Date</strong>: 11 September 2020<br>
+                                <strong>End Date</strong>: 16 October 2020<br>
+                                <strong>LG Registration Full</strong>
+                                <a href='https://iesjakarta.churchcenter.com/registrations/events/521073&ref=LinkToFaithLG'
+                                    target="_blank"><strong>CLICK HERE
+                                        TO REGISTER</strong></a>
+
+                            </div>
+                        </div>
+                    </div>
+                -->
+				</div>
+
+				<div class="flex-item_SpText">
+					<p>
+						For questions, please email Kristin at
+						<a href="mailto:kristin@iesjakarta.org">kristin@iesjakarta.org</a>
+						or WhatsApp from
+						<a
+							href="https://wa.me/628159460826?text=Hi%20Kristin.%20My%20name%20is%20_________.%20%0D%0AI%27d%20like%20to%20ask%20you%20
+                more%20about%20the%20upcoming%20IES%20Kids%20LG.%20%0D%0AMy%20child%27s%20name%20is%20_______________."
+							>here</a
+						>.
+					</p>
+				</div>
+			</div>
+			<br />
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	layout: "noHero",
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
