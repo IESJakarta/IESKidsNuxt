0653d8477733034399a226073d211ed08b9adffa
IESKids <63225321+IESKids@users.noreply.github.com>
2021-02-02 22:18:20 +0700

    Add files via upload

---
+++ layouts/noHero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex-container_Base_A">
+    <div class="topBar">
+      <Nav />
+    </div>
+    <Nuxt />
+  </div>
+</template>
+
+
+<style scoped>
+    @import '~assets/css/stylesheet.css';
+.topBar {
+ overflow: hidden;
+ position: fixed;
+ top: 0;
+ width: 100%; 
+ background-color: rgb(46, 46, 46);
+}
+</style>
