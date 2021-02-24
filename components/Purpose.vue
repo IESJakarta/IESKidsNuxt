1f1b8b3aa76f9830c23426f007ab8711d091ccc5
IESKids <63225321+IESKids@users.noreply.github.com>
2021-02-24 10:46:07 +0700

    updateing components directory

---
+++ components/Purpose.vue
@@ -0,0 +1,14 @@
+<template>
+    	<div>
+			Helping parents raise their kids in Christ.<br />
+			Developing ministers in the Kingdom of God.
+		</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
